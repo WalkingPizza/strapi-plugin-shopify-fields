4c9ab2d87e3cae6c03dbf9ed5e337428f58bd0cd
WalkingPizza <kevin.iatauro@gmail.com>
2022-09-28 00:30:07 +0200

    Dynamic API version based on plugin settings

--- server/services/shopify.js
+++ server/services/shopify.js
@@ -8,7 +8,7 @@ module.exports = ({ strapi }) => {
   const client = new Shopify({
     shopName: strapi.plugin(pluginId).config('shopName'),
     accessToken: strapi.plugin(pluginId).config('accessToken'),
-    apiVersion: '2022-07',
+    apiVersion: strapi.plugin(pluginId).config('apiVersion'),
     autoLimit: true,
   });
 
@@ -21,14 +21,6 @@ module.exports = ({ strapi }) => {
     const product = await client.product.get(id);
     return transformResponse(product, params);
   };
-
-  // const refresh = async (data) => {
-  //   if (Array.isArray(data)) {
-  //   }
-
-  //   const product = await getProduct(data.id);
-  //   return transformResponse(product, data.meta.params);
-  // };
 
   return { getProduct, getProducts };
 };
